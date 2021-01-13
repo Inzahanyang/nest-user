b1a2c2494d17ede18570df5e7ad220c760a5a4ea
inzahan <inzahanyang@gmail.com>
2021-01-14 00:51:01 +0900

    user-module

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,59 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { IsEmail, IsEnum } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class User extends CoreEntity {
+  @Column()
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @Field(type => String)
+  password: string;
+
+  @Column()
+  @Field(type => UserRole)
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(aPassword, this.password);
+      return ok;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
+}
